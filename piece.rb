b0f5d1cb0ab850c11fe5d655d5978e86963d841a
Codyz <codyz@appacademy.io>
2013-04-26 17:19:03 -0400

    redid the delta constants for piece class, to incorporate kings. Gonna make the king_me method now, to promote.

--- piece.rb
+++ piece.rb
@@ -7,21 +7,28 @@ class Piece
   S_DELTAS_RED = [[-1,1], [-1,-1]]
   J_DELTAS_RED = [[-2,2], [-2,-2]]
   # black pawn's moves
-  S_DELTAS_RED = [[-1,1], [-1,-1]]
-  J_DELTAS_RED = [[-2,2], [-2,-2]]
+  S_DELTAS_BLACK = [[1,1], [1,-1]]
+  J_DELTAS_BLACK = [[2,2], [2,-2]]
+  # king piece's moves
+  S_DELTAS_KING = S_DELTAS_RED + S_DELTAS_BLACK
+  J_DELTAS_KING = J_DELTAS_RED + J_DELTAS_BLACK
 
 
   def initialize(color, pos, board)
     @color = color
     @pos = pos
     @board = board
+    @promoted = false
   end
 
   def slide_set
     start = @pos
     cur_x, cur_y = start
-    @color == :B ? x = 1 : x = -1
-    moves = [[x,1], [x,-1]]
+    if !@promoted
+      moves = @color == :B ? S_DELTAS_BLACK : S_DELTAS_RED
+    else
+      moves = S_DELTAS_KING
+    end
     pos_slides = []
     moves.each do |dx, dy|
       pos_move = [cur_x + dx, cur_y + dy]
@@ -32,9 +39,11 @@ class Piece
 
   def jump_set
     start = @pos
-    cur_x, cur_y = start
-    @color == :B ? x = 2 : x = -2
-    moves = [[x,2], [x,-2]]
+    if !@promoted
+      moves = @color == :B ? J_DELTAS_BLACK : J_DELTAS_RED
+    else
+      moves = J_DELTAS_KING
+    end
     pos_jumps = []
     moves.each do |dx, dy|
       pos_move = [cur_x + dx, cur_y + dy]
